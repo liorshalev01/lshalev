8b5deb92856248cfc89f187714d61c0e91690bce
Lior Shalev <liorshalev01@users.noreply.github.com>
2025-09-20 12:17:13 +0300

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://liorshalev01.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://liorshalev01.github.io",
 });
